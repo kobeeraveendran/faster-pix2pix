5240179e592a359b2cc99c16183c0ba82a9739da
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-12-10 21:05:10 -0500

    added score calculator

---
+++ pytorch-resnets/score_calculator.py
@@ -0,0 +1,21 @@
+print('NOTE: scores may be entered as a space separated string or comma-separated string.')
+
+p2p_score = input('Pix2pix score: ')
+uresnet9_score = input('UResNet9 score: ')
+
+if ',' in p2p_score:
+    p2p_list = [int(x) for x in p2p_score.split(',')]
+else:
+    p2p_list = [int(x) for x in p2p_score.split(' ')]
+
+if ',' in uresnet9_score:
+    uresnet9_list = [int(x) for x in uresnet9_score.split(',')]
+else:
+    uresnet9_list = [int(x) for x in uresnet9_score.split(' ')]
+
+# calculate sums
+p2p_total = sum(p2p_list)
+uresnet9_total = sum(uresnet9_list)
+
+print('Pix2Pix total score: ' + str(p2p_total) + '/200; \tAverage per-image score: ' + str(int(round(p2p_total / 200 * 4))))
+print('UResNet9 total score: ' + str(uresnet9_total) + '/200; \tAverage per-image score: ' + str(int(round(uresnet9_total / 200 * 4))))
